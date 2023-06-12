b9c2fd770fa14dc00c44317620f40434d99377b8
Ameya <ameya.naik.storm@gmail.com>
2023-06-12 01:15:34 -0700

    pushed UI changes

--- Super Fox/Assets/Scripts/Finish.cs
+++ Super Fox/Assets/Scripts/Finish.cs
@@ -3,22 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Finish : MonoBehaviour
 {
+    [SerializeField] private Text popUp;
+
     private bool levelCompleted = false;
     private bool noBoss = true;
     private FinishPublisher finishPublisher;
-    private Collider2D trigger;
-    private SpriteRenderer rend;
     //Start is called before the first frame update
     void Start()
     {
-        trigger = GetComponent<Collider2D>();
-        rend = GetComponent<SpriteRenderer>();
         finishPublisher = GetComponent<FinishPublisher>();
-        trigger.enabled = false;
-        rend.enabled = false;
         finishPublisher.InitializeFinish(UpdateFinish);
         if (GameObject.FindGameObjectsWithTag("Boss").Length != 0)
         {
@@ -30,8 +27,21 @@ public class Finish : MonoBehaviour
     {
         if (collision.gameObject.name == "Player" && !levelCompleted)
         {
+            FindObjectOfType<SoundManager>().PlaySoundEffect("NextLevelDenied");
+            if (this.noBoss)
+            {
+                popUp.text = "Collect all the items to finish the level!";
+            }
+            else
+            {
+                popUp.text = "Collect all the items and defeat the bear to finish the level!";
+            }
+            popUp.gameObject.SetActive(true);
+            StartCoroutine(CloseMessageAfterSeconds(3f));
+        }
+        else if (collision.gameObject.name == "Player" && levelCompleted)
+        {
             FindObjectOfType<SoundManager>().PlaySoundEffect("NextLevel");
-            levelCompleted = true;
             Invoke("FinishLevel", 1f);
         }
     }
@@ -58,12 +68,17 @@ public class Finish : MonoBehaviour
         if ((bossDead && GameObject.FindGameObjectsWithTag("Cherry").Length == 0) 
         || (this.noBoss && GameObject.FindGameObjectsWithTag("Cherry").Length <= 1))
         {
-            trigger.enabled = true;
-            rend.enabled = true;
+            this.levelCompleted = true;
         }
         if (bossDead)
         {
             this.noBoss = true;
         }
+    }
+
+    IEnumerator CloseMessageAfterSeconds(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        popUp.gameObject.SetActive(false);
     }
 }
